269527d1cfaf30699e352e5f1badf9579f6925ea
Nitre19 <xvidalllicasmx@gmail.com>
2018-05-07 08:54:28 +0200

    yh
    hyh

--- js/remoteController.js
+++ js/remoteController.js
@@ -1,11 +1,9 @@
-var socket = io.connect('http://localhost:8081', {'forceNew': true});
+var socket = io.connect('https://node-jordiid96146808.codeanyapp.com/:3000', {'forceNew': true});
 
 socket.on('command', function(data){
 	console.log(data);
 	doAction(data);
 });
-
-socket.emit('a',"test");
 
 function doAction(data){
 /*	var command; 

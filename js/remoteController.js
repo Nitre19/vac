9c17440e4aca2cc9dde2f3144af6370167fec148
Nitre19 <xvidalllicasmx@gmail.com>
2018-05-08 14:01:35 +0200

    add ajax

--- js/remoteController.js
+++ js/remoteController.js
@@ -1,16 +1,28 @@
-var socket = io.connect('http://localhost:8081', {'forceNew': true});
+/*var socket = io.connect('http://localhost:8081', {'forceNew': true});
 
 socket.on('command', function(data){
 	console.log(data);
 	doAction(data);
 });
 
-function doAction(data){
-/*	var command; 
-	command = data.map(function(elem, index){
-		return ("${elem.command}");
-	}).join(" ");
-*/
 console.log(data);
 	document.getElementById(data).click();
+}
+*/
+function pedirAccion(){
+	setTimeout(function(){
+		var urlServer = "http://php-xvidalllicasmx738832.codeanyapp.com/?order=consultar";
+		conexion3=new XMLHttpRequest();
+		conexion3.onreadystatechange = procesar;
+		conexion3.open("GET", urlServer, true);
+		conexion3.send();
+			setTimeout(function(){
+				pedirAccion();
+			}, 1000);
+	}, 1000);
+}
+
+function procesar(){
+    console.log(conexion3.responseText);
+	document.getElementById(conexion3.responseText).click();
 }
